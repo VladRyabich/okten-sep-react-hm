8a0341230e98d3b4585b2e9ce71794c9828d4c04
Vlad Ryabich <107134617+VladRyabich@users.noreply.github.com>
2025-01-10 13:10:10 +0200

    - Homework 2 task 2

--- src/components/TodoComponent/TodoComponent.tsx
+++ src/components/TodoComponent/TodoComponent.tsx
@@ -7,7 +7,19 @@ interface TodoComponentProps {
 export const TodoComponent = ({todo}: TodoComponentProps) => {
     return (
         <>
-            <h3>{todo.title}</h3>
+            <div className={'p-7 ml-6 mt-2 border-2'}>
+                <h2>
+                    <span className={'font-bold mr-3'}>User ID:</span>
+                    <span className={'text-2xl'}>{todo.userId}</span>
+                </h2>
+                <h3>
+                    <span className={'font-bold mr-3'}>ID:</span>
+                    <span className={'text-2xl'}>{todo.id}</span>
+                </h3>
+                <p className={todo.completed ? 'complete' : 'not-complete'}>
+                    {todo.title}
+                </p>
+            </div>
         </>
     );
 };
