7f73a5680ba43600e84bb536af0501d3bc42610b
Vlad Ryabich <107134617+VladRyabich@users.noreply.github.com>
2025-01-20 13:37:34 +0200

    - Commit homework 04 task 01

---
+++ src/layouts/MenuLayout.tsx
@@ -0,0 +1,7 @@
+export const MenuLayout = () => {
+    return (
+        <>
+            <div></div>
+        </>
+    );
+};
--- src/layouts/PaginationLayout.tsx
+++ src/layouts/PaginationLayout.tsx
@@ -1,9 +1,11 @@
 import {Outlet} from "react-router-dom";
 import {PaginationComponent} from "../components/pagination-component/PaginationComponent.tsx";
+import {MenuLayout} from "./MenuLayout.tsx";
 
 export const PaginationLayout = () => {
     return (
         <>
+            <MenuLayout/>
             <Outlet/>
             <PaginationComponent/>
         </>
