579feba26dda5b6582334c328f35c292eb84e8de
Vlad Ryabich <107134617+VladRyabich@users.noreply.github.com>
2025-01-22 14:43:59 +0200

    - Сommit homework 05 task 01

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {RouterProvider} from "react-router";
 
 createRoot(document.getElementById('root')!)
-    .render(<App/>);
+    .render(<RouterProvider router={routes}>);
