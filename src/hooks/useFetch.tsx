9c898b34df16957337bd5416339d5de77234ca3d
Vlad Ryabich <107134617+VladRyabich@users.noreply.github.com>
2025-01-26 15:00:15 +0200

    - Сommit homework 07 task 02

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,15 @@
+import {useEffect, useState} from "react";
+
+export const useFetch = <T,>(url: string) => {
+    const [obj, setObj] = useState<T>();
+
+    useEffect(() => {
+        fetch(url)
+            .then(response => response.json())
+            .then(value => {
+                setObj(value);
+            });
+    }, []);
+
+    return obj;
+};
